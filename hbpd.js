4c259033fe33483936c16509edd39528294bbc97
Hen Huynh <139737865+Hen-Huynh@users.noreply.github.com>
2024-05-23 23:42:25 +0700

    Delete hbpd.js

--- hbpd.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('play-button').addEventListener('click', function() {
-    const message = document.getElementById('message');
-    message.textContent = 'Cục cứt';
-    message.style.display = 'block';
-});
